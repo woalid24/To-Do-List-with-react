3fae043033c51609a8641f13585d677a5149d3c6
woalid24 <woalid.s.hossin@gmail.com>
2023-07-18 00:28:05 +0600

    Todo Done buttone added

--- vite-project/src/MyComponents/TodoItem.jsx
+++ vite-project/src/MyComponents/TodoItem.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoItem = ({todo,onDelete}) => {
+    const [isDone, setIsDone] = useState(false);
+
+  const handleDoneClick = () => {
+    setIsDone(!isDone);
+  };
+  
     return ( 
         <div>
             <h4>{todo.title}</h4>
             <p>{todo.desc}</p>
             <button className='btn btn-sm btn-danger' onClick={()=>{onDelete(todo)}}>Delete</button>
+            <button className='btn btn-sm btn-primary mx-2' onClick={handleDoneClick}>
+        {isDone ? 'Todo Done' : 'Todo Undone'}
+      </button>
         </div>
     );
 };
